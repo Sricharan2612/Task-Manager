d3d08d2a46ab617aa133bd544a30d0382f216f34
Sricharan2612 <sricharany2000@gmail.com>
2024-04-10 18:06:15 +0530

    js code is added

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const taskInput = document.getElementById('taskInput');
-function addTask() { 
-    console.log(taskInput.value);
+const taskTable = document.querySelector('.task-table');
+function createTask() { 
+    if (taskInput.value !== "") {
+        addTaskToTable(taskInput.value);
+        taskInput.value = "";
+    } else { 
+        alert('Please enter a task');
+    }
 }
+
+function addTaskToTable(inputValue) { 
+    const tableRow = document.createElement('tr');
+    tableRow.setAttribute('class', 'data-row');
+    tableRow.innerHTML = `<td>${inputValue}</td>`;
+    console.log(inputValue);
+    taskTable.appendChild(tableRow);
+}
+
+document.querySelector('.add-btn').addEventListener('click', createTask);
